089282a57aad253def52ee338f892e9ef989e605
Michel S <64908337+michel-ds@users.noreply.github.com>
2022-11-09 16:29:20 +0000

    CLI arguments and code cleanup

--- src/make_kb.py
+++ src/make_kb.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 
 def load_entities(kb_data):
-    """ Create dictionary"""
+    """Create dictionary"""
     names = dict()
     descriptions = dict()
 
@@ -19,28 +19,32 @@ def load_entities(kb_data):
 
     return names, descriptions
 
+
 def get_embedding_dim(nlp):
-    if 'tok2vec' in nlp.config['components']:
-        return nlp.config['components']['tok2vec']['model']['encode']['width']
+    if "tok2vec" in nlp.config["components"]:
+        return nlp.config["components"]["tok2vec"]["model"]["encode"]["width"]
     else:
         raise NotImplementedError("Implement transformer embeddimg width code")
+
 
 def main(input_file, output_file, nlp_model, empty=True):
 
     # Load data and model
-    data = pd.read_csv(input_file,index_col=0)
-    kb_data=data[['id','name','desc']]
+    data = pd.read_csv(input_file, index_col=0)
+    kb_data = data[["id", "name", "desc"]]
     name_dict, desc_dict = load_entities(kb_data)
 
-    nlp = spacy.load(nlp_model, exclude=['lemmatizer', 'tagger', 'ner'])
+    nlp = spacy.load(nlp_model, exclude=["lemmatizer", "tagger", "ner"])
     embedding_dims = get_embedding_dim(nlp)
 
     ## Generate synthetic aliases
-    aliases_data = kb_data[kb_data['name'].duplicated(keep=False)].sort_values(['name'])
-    aliases_data['id'] = aliases_data['id'].astype(str)
+    aliases_data = kb_data[kb_data["name"].duplicated(keep=False)].sort_values(["name"])
+    aliases_data["id"] = aliases_data["id"].astype(str)
 
-    alias_dict = dict((alias, list(aliases_data.loc[aliases_data['name'] == alias, 'id'].values))
-                       for alias in aliases_data['name'].unique())
+    alias_dict = dict(
+        (alias, list(aliases_data.loc[aliases_data["name"] == alias, "id"].values))
+        for alias in aliases_data["name"].unique()
+    )
 
     # Set up empty KB object and fill with entities
     kb = KnowledgeBase(vocab=nlp.vocab, entity_vector_length=embedding_dims)
@@ -50,49 +54,60 @@ def main(input_file, output_file, nlp_model, empty=True):
     if empty:
         for qid, desc in desc_dict.items():
             desc_enc = np.zeros(embedding_dims)
-            kb.add_entity(entity=qid, entity_vector=desc_enc, freq=342)  # 342 is an arbitrary value here
+            kb.add_entity(
+                entity=qid, entity_vector=desc_enc, freq=342
+            )  # 342 is an arbitrary value here
     else:
         descriptions = desc_dict.values()
         qids = desc_dict.keys()
         for qid, desc_doc in zip(qids, nlp.pipe(descriptions)):
             desc_enc = desc_doc.vector
-        kb.add_entity(entity=qid, entity_vector=desc_enc, freq=342)   # 342 is an arbitrary value here
-
-
+        kb.add_entity(
+            entity=qid, entity_vector=desc_enc, freq=342
+        )  # 342 is an arbitrary value here
 
     for qid, name in name_dict.items():
         if name not in alias_dict.keys():
-            kb.add_alias(alias=str(name), entities=[str(qid)], probabilities=[1])   # 100% prior probability P(entity|alias)
+            kb.add_alias(
+                alias=str(name), entities=[str(qid)], probabilities=[1]
+            )  # 100% prior probability P(entity|alias)
 
     ### REFACTOR?
     for alias_ in alias_dict.keys():
-        qids=alias_dict[alias_]
-        probs = [round(1/len(qids),2)-.01 for qid in qids]
-        kb.add_alias(alias=alias_, entities=qids, probabilities=probs)  # sum([probs]) should be <= 1 !
-
+        qids = alias_dict[alias_]
+        probs = [round(1 / len(qids), 2) - 0.01 for qid in qids]
+        kb.add_alias(
+            alias=alias_, entities=qids, probabilities=probs
+        )  # sum([probs]) should be <= 1 !
 
     # Save file
     kb.to_disk(output_file)
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
+
     output_file, nlp_model = None, None
     verbose = False
 
     parser = argparse.ArgumentParser(
-        prog='make_kb',
-        description='Create knowledge base from KB dataset.'
+        prog="make_kb", description="Create knowledge base from KB dataset."
     )
-    parser.add_argument("input_file", default=["data/kb_entities_full.csv"])
-    parser.add_argument('-o', '--out', action="store_const", const=output_file, help="Name of output file.",
-                        default="kb")  # option that takes a value
-    parser.add_argument('-n', '--nlp', action="store_const", const=nlp_model, help="Name of the Spacy model",
-                        default="en_core_web_lg")
-    parser.add_argument('-e', '--empty', action='store_true', help="Fill embeddings with null vectors")
+    parser.add_argument("input_files", default=["data/kb_entities_full.csv"])
+    parser.add_argument(
+        "-o", "--out", nargs="?", help="Name of output file.", default="kb"
+    )
+    parser.add_argument(
+        "-n",
+        "--nlp",
+        action="store_const",
+        const=nlp_model,
+        help="Name of the Spacy model",
+        default="en_core_web_lg",
+    )
+    parser.add_argument(
+        "-e", "--empty", action="store_true", help="Fill embeddings with null vectors"
+    )
 
     args = parser.parse_args()
-    main(args.input_files, args.out, args.nlp_model, args.empty)
+    main(args.input_files, args.out, args.nlp, args.empty)
