d736d1fc759351d20b9c0a9746774820265e7c9e
Michel S <64908337+michel-ds@users.noreply.github.com>
2022-11-09 16:27:34 +0000

    Add data/candidate cleaning

--- src/prepare_training.py
+++ src/prepare_training.py
@@ -3,15 +3,33 @@ from sklearn.model_selection import train_test_split
 import spacy
 from spacy.tokens import DocBin, Span
 
+
+def find_no_candidates(row, no_candidate_sub_str=["NEL", "NER"]):
+    return any(sub_str in " ".join(row["accept"]) for sub_str in no_candidate_sub_str)
+
+
+def find_best_candidate(row):
+    best_candidate_d = {}
+    for option_id in row["accept"]:
+        for candidate in list(filter(lambda x: option_id == x["id"], row["options"])):
+            candidate_len = len(candidate["html"].split("a>:")[1])
+            best_candidate_d[option_id] = candidate_len
+    return [max(best_candidate_d, key=best_candidate_d.get)]
+
+
 def clean_data(df):
-    """ Implement data cleaning. """
+    """Implement data cleaning."""
+    df = df[df["accept"].apply(len) != 0]
+    df = df[~df.apply(find_no_candidates, axis=1)]
     return df
 
 
 def make_doc(example, nlp):
-    """ Make spacy document from prodigy example (dict)"""
+    """Make spacy document from prodigy example (dict)"""
     sentence = example["text"]
     gold_ids = []
+    # import pdb
+    # pdb.set_trace()
     if example["answer"] == "accept":
         QID = example["accept"][0]
         doc = nlp.make_doc(sentence)
@@ -28,21 +46,24 @@ def make_doc(example, nlp):
             doc[i].is_sent_start = i == 0
         return doc
 
-def main(input_files, out_stem, nlp_model = 'en_core_web_lg', verbose=False):
-    inputs = [
-        pandas.read_json(inp, lines=True)
-        for inp in input_files
-    ]
+
+def main(input_files, out_stem, nlp_model="en_core_web_lg", verbose=False):
+    inputs = [pandas.read_json(inp, lines=True) for inp in input_files]
     df = pandas.concat(inputs)
 
     # Clean data
     df = clean_data(df)
 
-    # Split datasets and ensure no paragraph is split between train and dev
-    index_train, index_test = train_test_split(df['_input_hash'].unique(), test_size=0.4, random_state=14)
+    # Surface only one (the best candidate)
+    df["accept"] = df.apply(find_best_candidate, axis=1)
 
-    df_train = df[df['_input_hash'].isin(index_train)]
-    df_test = df[df['_input_hash'].isin(index_test)]
+    # Split datasets and ensure no paragraph is split between train and dev
+    index_train, index_test = train_test_split(
+        df["_input_hash"].unique(), test_size=0.4, random_state=14
+    )
+
+    df_train = df[df["_input_hash"].isin(index_train)]
+    df_test = df[df["_input_hash"].isin(index_test)]
 
     # Make docs
     nlp = spacy.load(nlp_model, exclude="parser, tagger")
@@ -59,30 +80,42 @@ def main(input_files, out_stem, nlp_model = 'en_core_web_lg', verbose=False):
         test_docbin.add(doc)
 
     # Output training data in .spacy format
-    train_corpus = f'{out_stem}_train.spacy'
-    test_corpus = f'{out_stem}_test.spacy'
+    train_corpus = f"{out_stem}_train.spacy"
+    test_corpus = f"{out_stem}_test.spacy"
 
     train_docbin.to_disk(train_corpus)
     test_docbin.to_disk(test_corpus)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
-    input_files, output_stem = None, None
-    nlp_model = None
+
     verbose = False
 
     parser = argparse.ArgumentParser(
-        prog='prepare_training',
-        description='Prepare training dataset from annotations.'
-        )
-    parser.add_argument("input_files", nargs='+', default=["data/el_session1.json", "data/el_session2.json"])
-    parser.add_argument('-o', '--out', action="store_const", const=output_stem, help="Name structure of output files.",
-                        default="data/el")
-    parser.add_argument('-n', '--nlp', action="store_const", const=nlp_model, help="Name of the Spacy model",
-                        default="en_core_web_lg")
-    parser.add_argument('-v', '--verbose', action='store_true')
+        prog="prepare_training",
+        description="Prepare training dataset from annotations.",
+    )
+    parser.add_argument(
+        "input_files",
+        nargs="+",
+        default=["data/el_session_1.jsonl", "data/el_session_2.jsonl"],
+    )
+    parser.add_argument(
+        "-o",
+        "--out",
+        nargs="?",
+        help="Name structure of output files.",
+        default="data/el",
+    )
+    parser.add_argument(
+        "-n",
+        "--nlp",
+        nargs="?",
+        help="Name of the Spacy model",
+        default="en_core_web_lg",
+    )
+    parser.add_argument("-v", "--verbose", action="store_true")
 
     args = parser.parse_args()
-    main(args.input_files, args.out, args.verbose)
+    main(args.input_files, args.out, args.nlp, args.verbose)
